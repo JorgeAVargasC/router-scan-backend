6604bd743cda77cc7fda2b4a8eca4125964ef7ea
JorgeVargasConnectUp <jorge.vargas@connectupweb.com>
2023-08-19 12:09:20 -0500

    [FIX] issue

--- app.py
+++ app.py
@@ -33,15 +33,16 @@ def scan():
     scan_results_adapted = data_adapter(scan_results, gateway)
     scan_results_adapted = obtain_isp_info_from_api(scan_results_adapted)
     save_results_as_json(scan_results_adapted, '2-scan_results_adapted.json')
+    collection = db_connection()
 
     if len(scan_results_adapted['vulnerabilities']) == 0:
+        save_results_in_db(collection, scan_results_adapted)
         return jsonify(scan_results_adapted)
     
     scan_results_adapted_cve_info = obtain_cve_info_from_api(scan_results_adapted)  
     
     save_results_as_json(scan_results_adapted_cve_info, '3-scan_results_adapted_cve_info.json')
     
-    collection = db_connection()
     save_results_in_db(collection, scan_results_adapted_cve_info)
     
     return jsonify(scan_results_adapted_cve_info)
