d892d1374f78767e270787cb0895ba0601750cdc
JorgeAVargasC <javargas1209@gmail.com>
2023-09-09 13:04:52 -0500

    [UPDATE] reports

--- api/reports/get_top_vendor_cve.py
+++ api/reports/get_top_vendor_cve.py
@@ -1,3 +1,83 @@
+# from config.get_db_connection import get_db_connection
+# from flask import jsonify
+
+
+# def get_top_vendor_cve():
+#     try:
+#         db = get_db_connection()
+#         collection = db["router_scan_results"]
+
+#         # * ==================== (VENDOR) ===================== *#
+
+#         unique_vendors = collection.distinct("vendor")
+
+#         vendors_data = {}
+#         for vendor in unique_vendors:
+#             vendors_data[vendor] = 0
+
+#         pipeline = [
+#             {
+#                 "$group": {
+#                     "_id": "$ip",
+#                     "ip_count": {"$sum": 1},
+#                     "root": {"$addToSet": "$$ROOT"},
+#                 }
+#             },
+#             {"$match": {"ip_count": 1}},
+#             {"$unwind": "$root"},
+#             {
+#                 "$group": {
+#                     "_id": "$root.vendor",
+#                     "vendor_count": {"$sum": 1},
+#                     "cve": {"$addToSet": "$root.vulnerabilities.id"},
+
+#                 }
+#             },
+#             {
+#                 "$project": {
+#                     "_id": 1,
+#                     "vendor_count": 1,
+#                     "cve": {
+#                         "$reduce": {
+#                             "input": "$cve",
+#                             "initialValue": [],
+#                             "in": {"$setUnion": ["$$value", "$$this"]},
+#                         }
+#                     },
+#                     "cve_count": {
+#                         "$size": {
+#                             "$reduce": {
+#                                 "input": "$cve",
+#                                 "initialValue": [],
+#                                 "in": {"$setUnion": ["$$value", "$$this"]},
+#                             }
+#                         }
+#                     },
+#                 }
+#             },
+#             {
+#                 "$sort": {
+#                     "cve_count": -1,
+#                 }
+#             },
+#         ]
+
+#         vendors_data = list(collection.aggregate(pipeline))
+
+#         return (
+#             jsonify(
+#                 {"count": len(vendors_data), "data": vendors_data},
+#             ),
+#             200,
+#         )
+
+#     except Exception as e:
+#         print(e)
+#         return jsonify({"message": f"Error: {e}" }), 500
+
+
+# * NEW
+
 from config.get_db_connection import get_db_connection
 from flask import jsonify
 
@@ -7,13 +87,12 @@ def get_top_vendor_cve():
         db = get_db_connection()
         collection = db["router_scan_results"]
 
-        # * ==================== (VENDOR) ===================== *#
-
+        # Obtener los datos únicos de los proveedores (vendors)
         unique_vendors = collection.distinct("vendor")
 
         vendors_data = {}
         for vendor in unique_vendors:
-            vendors_data[vendor] = 0
+            vendors_data[vendor] = []
 
         pipeline = [
             {
@@ -29,7 +108,7 @@ def get_top_vendor_cve():
                 "$group": {
                     "_id": "$root.vendor",
                     "vendor_count": {"$sum": 1},
-                    "cve": {"$addToSet": "$root.vulnerabilities.id"},
+                    "cve": {"$addToSet": "$root.vulnerabilities"},
                 }
             },
             {
@@ -43,34 +122,32 @@ def get_top_vendor_cve():
                             "in": {"$setUnion": ["$$value", "$$this"]},
                         }
                     },
-                    "cve_count": {
-                        "$size": {
-                            "$reduce": {
-                                "input": "$cve",
-                                "initialValue": [],
-                                "in": {"$setUnion": ["$$value", "$$this"]},
-                            }
-                        }
-                    },
                 }
             },
             {
+                "$unset": "cve.recommendations",  # Elimina la clave "recommendations" del campo "cve"
+            },
+            {
+                "$unset": "cve.summary",
+            },
+            {
+                "$unset": "cve.published",
+            },
+            {
+                "$unset": "cve.modified",
+            },
+            {
                 "$sort": {
-                    "cve_count": -1,
+                    "vendor_count": -1,
                 }
             },
         ]
 
         vendors_data = list(collection.aggregate(pipeline))
-        # vendors_data.sort(key=lambda x: x["cve_count"], reverse=True)
 
-        return (
-            jsonify(
-                {"count": len(vendors_data), "data": vendors_data},
-            ),
-            200,
-        )
+        return jsonify({"count": len(vendors_data), "data": vendors_data}), 200
 
     except Exception as e:
         print(e)
-        return jsonify({"message": "Error"}), 500
+
+        return jsonify({"message": f"Error: {e}"}), 500
